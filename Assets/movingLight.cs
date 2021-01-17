e9d7c5180e18e881efd599996e03a560f8a9d956
wkalinski <wkalinski6@gmail.com>
2021-01-17 13:10:58 +0100

    Add light motion

---
+++ Assets/movingLight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingLight : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var light = gameObject.GetComponent<movingLight>();
+        light.transform.eulerAngles = new Vector3(
+            light.transform.eulerAngles.x,
+            light.transform.eulerAngles.y + 0.5f,
+            light.transform.eulerAngles.z
+        );
+    }
+}
